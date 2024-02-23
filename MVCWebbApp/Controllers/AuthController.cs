9bfcf349e733035b396a97bfa921ec28a3b76137
Johan <gunbald@live.se>
2024-02-23 17:26:34 +0100

    signin completed

--- MVCWebbApp/Controllers/AuthController.cs
+++ MVCWebbApp/Controllers/AuthController.cs
@@ -23,4 +23,29 @@ public class AuthController : Controller
         return RedirectToAction("SignIn", "Auth");
     }
 
+
+    [Route("/signin")]
+    [HttpGet]
+    public IActionResult SignIn()
+    {
+        var viewModel = new SignInViewModel();
+        return View(viewModel);
+    }
+
+    [Route("/signin")]
+    [HttpPost]
+    public IActionResult SignIn(SignInViewModel viewModel)
+    {
+        if (!ModelState.IsValid)
+            return View(viewModel);
+
+        //var result = _authService.SignIn(viewModel.Form);
+        //if (result)
+        //    return RedirectToAction("Account", "Index");
+
+        viewModel.ErrorMessage = "Incorrect email or password";
+        return View(viewModel);
+
+    }
+
 }
