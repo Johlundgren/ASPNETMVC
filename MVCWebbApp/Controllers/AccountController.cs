af1b19e10a5197f65f624e85804d16e9e399ea45
Johan <gunbald@live.se>
2024-02-23 17:37:15 +0100

    account init

--- MVCWebbApp/Controllers/AccountController.cs
+++ MVCWebbApp/Controllers/AccountController.cs
@@ -4,5 +4,10 @@ namespace MVCWebbApp.Controllers;
 
 public class AccountController : Controller
 {
+    //private readonly AccountService _accountService;
 
+    //public AccountController(AccountService accountService)
+    //{
+    //    _accountService = accountService;
+    //}
 }
